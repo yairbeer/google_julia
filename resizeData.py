68b45dd283d018beb5096f0a4d55f2351a3bdfbc
yaia <yaia1309@gmail.com>
2016-04-13 14:07:14 +0300

    added image data generator

--- resizeData.py
+++ resizeData.py
@@ -6,23 +6,23 @@ import os
 # Set path of data files
 path = "data"
 
-if not os.path.exists(path + "/trainResized"):
-    os.makedirs(path + "/trainResized")
-if not os.path.exists(path + "/testResized"):
-    os.makedirs(path + "/testResized")
+if not os.path.exists(path + "/trainResized64"):
+    os.makedirs(path + "/trainResized64")
+if not os.path.exists(path + "/testResized64"):
+    os.makedirs(path + "/testResized64")
 
-img_size = 30
+img_size = 64
 
 trainFiles = glob.glob(path + "/train/*")
 for i, nameFile in enumerate(trainFiles):
     image = imread(nameFile)
     imageResized = resize(image, (img_size, img_size))
-    newName = "/".join(nameFile.split("/")[:-1]) + "Resized40/" + nameFile.split("/")[-1]
+    newName = "/".join(nameFile.split("/")[:-1]) + "Resized64/" + nameFile.split("/")[-1]
     imsave(newName, imageResized)
 
 testFiles = glob.glob(path + "/test/*")
 for i, nameFile in enumerate(testFiles):
     image = imread(nameFile)
     imageResized = resize(image, (img_size, img_size))
-    newName = "/".join(nameFile.split("/")[:-1]) + "Resized40/" + nameFile.split("/")[-1]
+    newName = "/".join(nameFile.split("/")[:-1]) + "Resized64/" + nameFile.split("/")[-1]
     imsave(newName, imageResized)
